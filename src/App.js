48e4cfaf6cdb89030b9438bd1308a008cde18ae4
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-05-05 14:08:18 +0530

    fetch-data-fetch-keyword-list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Client from "./components/Client";
 import User from "./components/User";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       Hello
       <User />
+      <Client />
     </div>
   );
 }
